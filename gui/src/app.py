ab83f104a3d3179edbcf8d2ee17a4c6e194a48be
etcadinfinitum <lizzy.presland@gmail.com>
2019-10-12 15:26:16 -0700

    Add bare-bones app.py file for Flask app

---
+++ gui/src/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, url_for, request, render_template, redirect
+import time
+
+@app.route('/')
+@app.route('/index.html')
+def landing():
+    # This is where we would have the image upload 
+    # and invoke the Vision service to transform 
+    # the image.
+    return 'Hello world!'
+
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0')
